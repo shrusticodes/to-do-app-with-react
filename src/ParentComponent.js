71273782ae5a1873fa81dc22a4db2bb10c309dec
Shrusti <89652827+shrusticodes@users.noreply.github.com>
2023-06-21 17:37:10 +0530

    filters added

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 function ParentComponent() {
   const [toDoTasks, setToDoTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
   const addTaskHandler = useCallback((value) => {
     if (value === '') {
       alert("Fill the input box to add a task");
@@ -15,13 +16,13 @@ function ParentComponent() {
       setToDoTasks(updatedTaskList);
       console.log("Task added");
     }
-  }, [toDoTasks]);
+  }, [toDoTasks,setToDoTasks]);
 
   const onDeleteHandler = useCallback((id) => {
     const updatedTaskList = toDoTasks.filter(task => task.id !== id);
     setToDoTasks(updatedTaskList);
     console.log("Task deleted");
-  }, [toDoTasks,setToDoTasks]);
+  }, [toDoTasks, setToDoTasks]);
 
   const isTaskDoneHandler = useCallback((id) => {
     const updatedTaskList = [...toDoTasks];
@@ -29,7 +30,7 @@ function ParentComponent() {
     updatedTaskList[taskIndex].isTaskDone = !updatedTaskList[taskIndex].isTaskDone;
     setToDoTasks(updatedTaskList);
     console.log("Task checked");
-  }, [toDoTasks]);
+  }, [toDoTasks,setToDoTasks]);
 
   const onTaskUpdate = useCallback((id, event) => {
     const updatedTaskList = [...toDoTasks];
@@ -37,26 +38,44 @@ function ParentComponent() {
     updatedTaskList[taskIndex].taskName = event.target.value;
     setToDoTasks(updatedTaskList);
     console.log("Task updated");
-  }, [toDoTasks]);
+  }, [toDoTasks,setToDoTasks]);
 
-  const listItems = toDoTasks.map((task) => (
+  const filterTasks = useCallback(() => {
+    if (filter === "active") {
+      return toDoTasks.filter(task => !task.isTaskDone);
+    } else if (filter === "completed") {
+      return toDoTasks.filter(task => task.isTaskDone);
+    }
+    return toDoTasks;
+  }, [toDoTasks, filter]);
+
+  const handleFilterChange = (filterValue) => {
+    setFilter(filterValue);
+  };
+
+  const listItems = filterTasks().map((task) => (
     <TaskComponent
       key={task.id}
       task={task}
       onDelete={onDeleteHandler}
-      isTaskDone={isTaskDoneHandler}
+      onTaskDone={isTaskDoneHandler}
       onUpdate={onTaskUpdate}
     />
   ));
+  console.log(toDoTasks)
 
   return (
- 
     <div className="mainDiv">
       <h2>To Do List</h2>
       <AddTask addTask={addTaskHandler} />
+      <div className='filter-div'>
+        <button className='btn-style filter' onClick={()=>handleFilterChange("all")}>All Tasks</button>
+        <button className='btn-style filter' onClick={()=>handleFilterChange("active")}>Active</button>
+        <button className='btn-style filter' onClick={()=>handleFilterChange("completed")}>Completed</button>
+      </div>
       <ul>
         {listItems}
-    </ul>
+      </ul>
     </div>
   );
 }
--- src/TaskComponent.js
+++ src/TaskComponent.js
@@ -1,18 +1,18 @@
 import React from 'react'
-function TaskComponent({task, onDelete, isTaskDone, onUpdate}) {
-    const {id, taskName, onCheck} = task;
+function TaskComponent({task, onDelete, onTaskDone, onUpdate}) {
+    const {id, taskName, isTaskDone} = task;
     const handleOnDelete=()=>{
         onDelete(id);
     }
     const handleChange = ()=>{
-        isTaskDone(id);
+        onTaskDone(id);
     }
     const handleOnUpdate = (event) =>{
         onUpdate(id,event);
     }
     return (
         <li>
-            <input type='checkbox' checked={onCheck} onClick={handleChange} />
+            <input type='checkbox' checked={isTaskDone} onChange={handleChange} />
             <input type="text" value={taskName} onChange={handleOnUpdate}/>
             <button className="btn-style" onClick={handleOnDelete}>Delete</button>
         </li>
